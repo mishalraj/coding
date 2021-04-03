87ae37a5e3cdeca2a0ab8c449a0407af199d92e5
mishalraj <mishalraj17@gmail.com>
2021-04-03 17:35:26 +0530

    merging arrays

--- MergingArrays.java
+++ MergingArrays.java
@@ -1,10 +1,27 @@
 public class MergingArrays {
+
+    static int arr1[]= {1, 5, 9, 10, 15, 20};
+    static int arr2[]= {2, 3, 8, 13};
+
     public static void main(String[] args) {
 
-        int arr1[]= {1, 5, 9, 10, 15, 20};
-        int arr2[]={2, 3, 8, 13};
+        int m = arr1.length;
+        int n = arr2.length;
 
+        sortTwoArrays(m,n);
 
+        for(int i=0;i<m;i++)
+        {
+            System.out.print(arr1[i]);
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(arr2[i]);
+        }
+    }
+
+    private static void sortTwoArrays(int m, int n) {
 
     }
 }
