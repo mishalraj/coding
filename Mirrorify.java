e203a05cb16701a49e5bd2f254e951ace58f5b4b
mishalraj <mishalraj17@gmail.com>
2021-04-14 20:43:18 +0530

    "Mirrorify"

---
+++ Mirrorify.java
@@ -0,0 +1,51 @@
+public class Mirrorify {
+    static class Node
+    {
+        Node left ;
+        Node right ;
+        int data;
+    }
+
+    static void inOrder(Node root){
+        if(root==null)
+            return;
+
+        inOrder(root.left);
+        System.out.print(root.data+" ");
+        inOrder(root.right);
+    }
+
+    static Node createTree(int data)
+    {
+        Node newNode= new Node();
+        newNode.data= data;
+        newNode.left=newNode.right=null;
+        return newNode;
+    }
+
+    static Node mirrorify(Node root)
+    {
+        if(root==null)
+            return null;
+
+        Node newNode = createTree(root.data);
+        newNode.left= mirrorify(root.right);
+        newNode.right= mirrorify(root.left);
+        return newNode;
+    }
+
+    public static void main(String str[])
+    {
+        Node tree = createTree(5);
+        tree.left = createTree(3);
+        tree.right = createTree(6);
+        tree.left.left = createTree(2);
+        tree.left.right = createTree(4);
+
+        inOrder(tree);
+        Node mirror = mirrorify(tree);
+        System.out.println();
+        inOrder(mirror);
+    }
+
+}
